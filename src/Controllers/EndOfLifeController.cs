5d51930b84b136f8b60429e754344c6567dfdf33
Doug Murphy <doug.murphy82@gmail.com>
2021-11-17 21:48:24 -0500

    Switch to EOL NuGet package

--- src/Controllers/EndOfLifeController.cs
+++ src/Controllers/EndOfLifeController.cs
@@ -1,47 +1,45 @@
-﻿using EndOfLifeApi.Enums;
-using EndOfLifeApi.Helpers;
-using EndOfLifeApi.Models;
-using Microsoft.AspNetCore.Http;
+﻿using DougMurphy.TargetFrameworks.EndOfLife.Enums;
+using DougMurphy.TargetFrameworks.EndOfLife.Helpers;
+using DougMurphy.TargetFrameworks.EndOfLife.Models;
 using Microsoft.AspNetCore.Mvc;
-using System;
 
-namespace EndOfLifeApi.Controllers {
-	[ApiController]
-	[Route("[controller]")]
-	public class EndOfLifeController : ControllerBase {
-		/// <summary>Determine whether or not given Target Framework Moniker(s) is/are currently end of life.</summary>
-		/// <param name="tfm">The TFM to check. Can be a singular TFM or a semicolon-delimited list of TFMs.</param>
-		/// <returns></returns>
-		[HttpGet]
-		[Route("check-eol/{tfm}")]
-		[ProducesResponseType(typeof(TargetFrameworkCheckResponse), StatusCodes.Status200OK)]
-		[ProducesResponseType(StatusCodes.Status204NoContent)]
-		[ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
-		[ProducesDefaultResponseType]
-		public IActionResult CheckTargetFrameworkForEndOfLife(string tfm) {
-			ArgumentNullException.ThrowIfNull(tfm);
+namespace EndOfLifeApi.Controllers;
 
-			TargetFrameworkCheckResponse endOfLifeResults = TargetFrameworkEndOfLifeHelper.CheckTargetFrameworkForEndOfLife(tfm);
-			if (endOfLifeResults.EndOfLifeTargetFrameworks.Length == 0) {
-				return NoContent();
-			}
+[ApiController]
+[Route("[controller]")]
+public class EndOfLifeController : ControllerBase {
+	/// <summary>Determine whether or not given Target Framework Moniker(s) is/are currently end of life.</summary>
+	/// <param name="tfm">The TFM to check. Can be a singular TFM or a semicolon-delimited list of TFMs.</param>
+	/// <returns></returns>
+	[HttpGet]
+	[Route("check-eol/{tfm}")]
+	[ProducesResponseType(typeof(TargetFrameworkCheckResponse), StatusCodes.Status200OK)]
+	[ProducesResponseType(StatusCodes.Status204NoContent)]
+	[ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+	[ProducesDefaultResponseType]
+	public IActionResult CheckTargetFrameworkForEndOfLife(string tfm) {
+		ArgumentNullException.ThrowIfNull(tfm);
 
-			return Ok(endOfLifeResults);
+		TargetFrameworkCheckResponse endOfLifeResults = TargetFrameworkEndOfLifeHelper.CheckTargetFrameworkForEndOfLife(tfm);
+		if (endOfLifeResults.EndOfLifeTargetFrameworks.Count == 0) {
+			return NoContent();
 		}
 
-		/// <summary>Get a list of all Target Framework Monikers that are currently end of life or will be end of life within a specified timeframe.</summary>
-		/// <param name="timeframeUnit">The unit of the timeframe. eg. day, week, etc.</param>
-		/// <param name="timeframeAmount">How many of the unit to forecast ahead for. eg. 3 weeks, 4 months, etc.</param>
-		/// <returns></returns>
-		[HttpGet]
-		[Route("get-all-eol")]
-		[ProducesResponseType(typeof(TargetFrameworkCheckResponse), StatusCodes.Status200OK)]
-		[ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
-		[ProducesDefaultResponseType]
-		public IActionResult GetEndOfLifeTargetFrameworks(TimeframeUnit? timeframeUnit, byte? timeframeAmount) {
-			TargetFrameworkCheckResponse endOfLifeResults = TargetFrameworkEndOfLifeHelper.GetAllEndOfLifeTargetFrameworkMonikers(timeframeUnit, timeframeAmount);
+		return Ok(endOfLifeResults);
+	}
 
-			return Ok(endOfLifeResults);
-		}
+	/// <summary>Get a list of all Target Framework Monikers that are currently end of life or will be end of life within a specified timeframe.</summary>
+	/// <param name="timeframeUnit">The unit of the timeframe. eg. day, week, etc.</param>
+	/// <param name="timeframeAmount">How many of the unit to forecast ahead for. eg. 3 weeks, 4 months, etc.</param>
+	/// <returns></returns>
+	[HttpGet]
+	[Route("get-all-eol")]
+	[ProducesResponseType(typeof(TargetFrameworkCheckResponse), StatusCodes.Status200OK)]
+	[ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+	[ProducesDefaultResponseType]
+	public IActionResult GetEndOfLifeTargetFrameworks(TimeframeUnit timeframeUnit, byte timeframeAmount) {
+		TargetFrameworkCheckResponse endOfLifeResults = TargetFrameworkEndOfLifeHelper.GetAllEndOfLifeTargetFrameworkMonikers(timeframeUnit, timeframeAmount);
+
+		return Ok(endOfLifeResults);
 	}
 }
--- src/Filters/HttpResponseExceptionFilter.cs
+++ src/Filters/HttpResponseExceptionFilter.cs
@@ -1,18 +1,17 @@
-﻿using EndOfLifeApi.Exceptions;
+﻿using DougMurphy.TargetFrameworks.EndOfLife.Exceptions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
-using System;
 
-namespace EndOfLifeApi.Filters {
-	public class HttpResponseExceptionFilterAttribute : ExceptionFilterAttribute {
-		public override void OnException(ExceptionContext context) {
-			switch (context.Exception) {
-				case ArgumentException:
-				case TargetFrameworkUnknownException:
-					context.Result = new BadRequestObjectResult(new ProblemDetails {Detail = context.Exception.Message});
-					context.ExceptionHandled = true;
-					break;
-			}
+namespace EndOfLifeApi.Filters;
+
+public class HttpResponseExceptionFilterAttribute : ExceptionFilterAttribute {
+	public override void OnException(ExceptionContext context) {
+		switch (context.Exception) {
+			case ArgumentException:
+			case TargetFrameworkUnknownException:
+				context.Result = new BadRequestObjectResult(new ProblemDetails {Detail = context.Exception.Message});
+				context.ExceptionHandled = true;
+				break;
 		}
 	}
 }
