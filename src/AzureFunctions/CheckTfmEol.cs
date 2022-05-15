bcecc13410879df331740eb0243e77ce8ef160ba
Doug Murphy <doug.murphy82@gmail.com>
2022-05-15 13:46:59 -0400

    Add in Azure Functions support.

---
+++ src/AzureFunctions/CheckTfmEol.cs
@@ -0,0 +1,25 @@
+﻿using DougMurphy.TargetFrameworks.EndOfLife.Helpers;
+using DougMurphy.TargetFrameworks.EndOfLife.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+
+namespace EndOfLifeApi.AzureFunctions;
+
+public static class CheckTfmEol {
+	[FunctionName("CheckTfmEol")]
+	public static async Task<IActionResult> RunAsync([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "check-eol/{tfm:regex([a-zA-Z0-9.]{{3,}}):required}")] HttpRequest req,
+	                                                 string tfm,
+	                                                 ILogger log) {
+		log.LogInformation("Checking {TargetFrameworkMoniker} for EOL", tfm);
+
+		ArgumentNullException.ThrowIfNull(tfm);
+
+		TargetFrameworkCheckResponse endOfLifeResults = TargetFrameworkEndOfLifeHelper.CheckTargetFrameworkForEndOfLife(tfm);
+		if (endOfLifeResults.EndOfLifeTargetFrameworks.Count == 0) {
+			return new NoContentResult();
+		}
+
+		return new OkObjectResult(endOfLifeResults);
+	}
+}
---
+++ src/AzureFunctions/GetAllEolTfm.cs
@@ -0,0 +1,21 @@
+﻿using DougMurphy.TargetFrameworks.EndOfLife.Enums;
+using DougMurphy.TargetFrameworks.EndOfLife.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+
+namespace EndOfLifeApi.AzureFunctions;
+
+public static class GetAllEolTfm {
+	[FunctionName("GetAllEolTfm")]
+	public static async Task<IActionResult> RunAsync([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "get-all-eol")] HttpRequest req,
+	                                                 ILogger log) {
+		log.LogInformation("Getting all EOL TFMs with query params {QueryParams}", req.QueryString);
+
+		if (Enum.TryParse(req.Query["timeframeUnit"].ToString(), out TimeframeUnit timeframeUnit) && byte.TryParse(req.Query["timeframeAmount"], out byte timeframeAmount)) {
+			return new OkObjectResult(TargetFrameworkEndOfLifeHelper.GetAllEndOfLifeTargetFrameworkMonikers(timeframeUnit, timeframeAmount));
+		}
+
+		return new OkObjectResult(TargetFrameworkEndOfLifeHelper.GetAllEndOfLifeTargetFrameworkMonikers());
+	}
+}
